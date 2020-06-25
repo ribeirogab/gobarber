fb39b09e46e6b179b3e3236dcb223eb92900354e
ribeirogab <gblosr@gmail.com>
2020-06-25 20:13:36 -0300

    feat (mobile): SignIn and SignUp page

---
+++ appgobarber/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as GoBarber } from './app.json';
+
+AppRegistry.registerComponent(GoBarber, () => App);
